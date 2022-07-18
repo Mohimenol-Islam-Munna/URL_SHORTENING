40cb61094616f8332d91387658e3e910b692b55e
Mohimenol-Islam-Munna <munna.cse.pust@gmail.com>
2022-07-18 12:40:05 +0600

    cors problem solve

--- src/components/Shortening.js
+++ src/components/Shortening.js
@@ -2,53 +2,76 @@ import React, { useState } from "react";
 import axios from "axios";
 
 const Shortening = () => {
-  const [linkState, setLinkState] = useState("");
+  const [inputLink, setInputLink] = useState("");
+  const [inputError, setInputError] = useState({ status: false, message: "" });
+  const [shortLink, setShortLink] = useState({});
+  const [multipleShortLink, setMultipleShortLink] = useState([]);
 
-  const shortenLinkHandler = (event) => {
-    console.log("linkHandler ::", event.target.value);
-    setLinkState(event.target.value);
+  console.log("shortLink :go:", shortLink);
+  console.log("multipleShortLink :goo:", multipleShortLink);
+
+  const inputLinkHandler = (event) => {
+    setInputLink(event.target.value);
   };
 
-  const shortenApiHandler = async () => {
-    console.log("shortenApiHandler ::");
+  const createShortenLinkHandler = async () => {
+    console.log("shortenApiHandler :go:");
 
-    const data = {};
-    data.url = linkState;
-
-    console.log("data ::", data);
-
-    try {
-      let res = await axios.post("https://api.shrtco.de/v2/shorten", data, {
-        headers: {
-          "Content-type": "application/json",
-        },
+    if (inputLink === "") {
+      setInputError({
+        ...inputError,
+        status: true,
+        message: "Please Enter Valid Url",
       });
+    } else {
+      try {
+        let res = await axios.get("https://api.shrtco.de/v2/shorten", {
+          params: {
+            url: inputLink,
+          },
 
-      console.log("res ::", res);
-    } catch (err) {
-      console.log("err ::", err);
+          headers: {
+            "Content-type": "application/json",
+          },
+        });
+
+        setShortLink(res?.data?.result);
+        setMultipleShortLink([...multipleShortLink, res?.data?.result]);
+        setInputLink("");
+        setInputError({
+          ...inputError,
+          status: false,
+          message: "",
+        });
+      } catch (err) {
+        console.log("err ::", err);
+      }
     }
   };
 
   return (
-    <div>
-      <div className="bg-[#35323E] p-8 rounded-md flex gap-3">
+    <div className="bg-[#35323E] p-8">
+      <div className="rounded-md flex gap-3">
         <input
           className="p-3 border-3 border-red-600 rounded-lg flex-grow"
           type="text"
           name="link"
-          value={linkState}
-          onChange={(event) => shortenLinkHandler(event)}
+          value={inputLink}
+          onChange={(event) => inputLinkHandler(event)}
           placeholder="Enter Your Link"
         />
         <button
           className="bg-[#2ACFCF] p-3 px-3 rounded-lg"
-          onClick={shortenApiHandler}
+          onClick={createShortenLinkHandler}
         >
           Shorten It
         </button>
       </div>
-      <div></div>
+      <div>
+        {inputError.status && (
+          <p className="text-red-400 mt-2">{inputError.message}</p>
+        )}
+      </div>
     </div>
   );
 };
