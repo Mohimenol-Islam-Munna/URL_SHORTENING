ea2129be14ac2b880c7bb1f94ed7631e6d1a1251
Mohimenol-Islam-Munna <munna.cse.pust@gmail.com>
2022-07-18 13:33:40 +0600

    add short links to localstorage

--- src/components/Shortening.js
+++ src/components/Shortening.js
@@ -1,22 +1,16 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import axios from "axios";
 
 const Shortening = () => {
   const [inputLink, setInputLink] = useState("");
   const [inputError, setInputError] = useState({ status: false, message: "" });
-  const [shortLink, setShortLink] = useState({});
   const [multipleShortLink, setMultipleShortLink] = useState([]);
-
-  console.log("shortLink :go:", shortLink);
-  console.log("multipleShortLink :goo:", multipleShortLink);
 
   const inputLinkHandler = (event) => {
     setInputLink(event.target.value);
   };
 
   const createShortenLinkHandler = async () => {
-    console.log("shortenApiHandler :go:");
-
     if (inputLink === "") {
       setInputError({
         ...inputError,
@@ -35,7 +29,6 @@ const Shortening = () => {
           },
         });
 
-        setShortLink(res?.data?.result);
         setMultipleShortLink([...multipleShortLink, res?.data?.result]);
         setInputLink("");
         setInputError({
@@ -44,35 +37,71 @@ const Shortening = () => {
           message: "",
         });
       } catch (err) {
-        console.log("err ::", err);
+        setInputError({
+          ...inputError,
+          status: true,
+          message: "Please Enter Valid Url or Check Your Internet Connection",
+        });
       }
     }
   };
 
+  useEffect(() => {
+    if (multipleShortLink.length === 0) {
+      if (localStorage.multipleShortLink) {
+        setMultipleShortLink(
+          JSON.parse(localStorage.getItem("multipleShortLink"))
+        );
+      } else {
+        setMultipleShortLink([]);
+      }
+    }
+  }, []);
+
+  useEffect(() => {
+    if (multipleShortLink.length > 0) {
+      localStorage.setItem(
+        "multipleShortLink",
+        JSON.stringify(multipleShortLink)
+      );
+    }
+  }, [multipleShortLink]);
+
   return (
-    <div className="bg-[#35323E] p-8">
-      <div className="rounded-md flex gap-3">
-        <input
-          className="p-3 border-3 border-red-600 rounded-lg flex-grow"
-          type="text"
-          name="link"
-          value={inputLink}
-          onChange={(event) => inputLinkHandler(event)}
-          placeholder="Enter Your Link"
-        />
-        <button
-          className="bg-[#2ACFCF] p-3 px-3 rounded-lg"
-          onClick={createShortenLinkHandler}
-        >
-          Shorten It
-        </button>
+    <>
+      <div className="bg-[#35323E] p-8">
+        <div className="rounded-md flex gap-3">
+          <input
+            className="p-3 border-3 border-red-600 rounded-lg flex-grow"
+            type="text"
+            name="link"
+            value={inputLink}
+            onChange={(event) => inputLinkHandler(event)}
+            placeholder="Enter Your Link"
+          />
+          <button
+            className="bg-[#2ACFCF] p-3 px-3 rounded-lg"
+            onClick={createShortenLinkHandler}
+          >
+            Shorten It
+          </button>
+        </div>
+        <div>
+          {inputError.status && (
+            <p className="text-red-400 mt-2">{inputError.message}</p>
+          )}
+        </div>
       </div>
-      <div>
-        {inputError.status && (
-          <p className="text-red-400 mt-2">{inputError.message}</p>
-        )}
+      <div className="mt-5">
+        {multipleShortLink.length > 0
+          ? multipleShortLink.map((link, index) => (
+              <div key={index} className="bg-gray-300 p-8 mt-2">
+                <p>{link.short_link}</p>
+              </div>
+            ))
+          : ""}
       </div>
-    </div>
+    </>
   );
 };
 
